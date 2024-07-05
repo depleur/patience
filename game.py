0fddb0ab7004158f91d97d2534f50b89be6cdef3
Namit Arjaria <namitarjaria25@gmail.com>
2024-07-05 14:21:41 +0530

    chore: update movement and board canvas
    Signed-off-by: Namit Arjaria <namitarjaria25@gmail.com>

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
 import random
+import time
 
 
 class Card:
@@ -10,6 +11,7 @@ class Card:
         self.rank = rank
         self.color = "red" if suit in ["hearts", "diamonds"] else "black"
         self.image = image
+        self.cards_above = []  # Cards stacked on top of this card
 
 
 class PatienceGame:
@@ -17,7 +19,6 @@ class PatienceGame:
         self.master = master
         self.master.title("Patience Card Game")
 
-        # Define card dimensions at the beginning
         self.card_width = 80
         self.card_height = 120
 
@@ -25,14 +26,15 @@ class PatienceGame:
         self.create_menu()
         self.create_game_area()
         self.create_status_bar()
+        self.create_deal_button()
 
         self.card_images = self.load_card_images()
-        self.deck = self.create_deck()
         self.houses = [[] for _ in range(10)]
         self.end_houses = [[] for _ in range(4)]
         self.card_items = {}
-        self.setup_game()
-        self.display_cards()
+        self.drag_data = {"x": 0, "y": 0, "item": None}
+
+        self.status_var.set("Welcome to Patience! Click 'Deal Cards' to begin.")
 
     def center_window(self, width, height):
         screen_width = self.master.winfo_screenwidth()
@@ -95,14 +97,16 @@ class PatienceGame:
         status_bar.pack(side=tk.BOTTOM, fill=tk.X)
 
     def new_game(self):
-        self.status_var.set("New game started!")
-        self.setup_game()
-        self.display_cards()
+        self.houses = [[] for _ in range(10)]
+        self.end_houses = [[] for _ in range(4)]
+        self.card_items.clear()
+        self.game_canvas.delete("card")
+        self.status_var.set("New game started. Click 'Deal Cards' to begin.")
+        self.deal_button.config(state=tk.NORMAL)
 
     def restart_game(self):
-        self.status_var.set("Game restarted!")
-        self.setup_game()
-        self.display_cards()
+        self.new_game()
+        self.status_var.set("Game restarted. Click 'Deal Cards' to begin.")
 
     def load_card_images(self):
         suits = ["hearts", "diamonds", "clubs", "spades"]
@@ -129,22 +133,74 @@ class PatienceGame:
         random.shuffle(deck)
         return deck
 
-    def setup_game(self):
+    def create_deal_button(self):
+        self.deal_button = tk.Button(
+            self.master, text="Deal Cards", command=self.animated_deal
+        )
+        self.deal_button.pack(side=tk.BOTTOM, pady=10)
+        self.deal_button.config(state=tk.NORMAL)
+
+    def animated_deal(self):
+        self.deal_button.config(state=tk.DISABLED)  # Disable the button during dealing
+        self.status_var.set("Dealing cards...")
+
+        # Clear existing cards
         self.houses = [[] for _ in range(10)]
         self.end_houses = [[] for _ in range(4)]
         self.deck = self.create_deck()
 
         house_card_counts = [8, 8, 8, 7, 6, 5, 4, 3, 2, 1]
-        start_from_right = True
 
-        for house_idx, card_count in enumerate(house_card_counts):
-            if start_from_right:
-                house_position = 9 - house_idx
+        # Deal cards from right to left for the first iteration
+        for i in range(9, -1, -1):
+            for _ in range(house_card_counts[9 - i]):
+                self.houses[i].append(self.deck.pop())
+                self.display_cards()
+                self.master.update()
+                time.sleep(0.1)
+
+        # Deal remaining cards from left to right
+        current_house = 0
+        left_to_right = True
+        while self.deck:
+            if len(self.houses[current_house]) < 8:
+                self.houses[current_house].append(self.deck.pop())
+                self.display_cards()
+                self.master.update()
+                time.sleep(0.1)
+
+            if left_to_right:
+                current_house = (current_house + 1) % 10
+                if current_house == 0:
+                    left_to_right = False
             else:
-                house_position = house_idx
+                current_house = (current_house - 1) % 10
+                if current_house == 9:
+                    left_to_right = True
 
-            self.houses[house_position] = [self.deck.pop() for _ in range(card_count)]
-            start_from_right = not start_from_right
+        self.status_var.set("Cards dealt. Good luck!")
+        self.deal_button.config(
+            state=tk.DISABLED
+        )  # Keep the button disabled after dealing
+
+    # def setup_game(self):
+    #     self.houses = [[] for _ in range(10)]
+    #     self.end_houses = [[] for _ in range(4)]
+    #     self.deck = self.create_deck()
+    #     self.status_var.set("Welcome to Patience! Click 'Deal Cards' to begin.")
+
+    #     house_card_counts = [8, 8, 8, 7, 6, 5, 4, 3, 2, 1]
+
+    #     # Deal cards from right to left for the first iteration
+    #     for i in range(9, -1, -1):
+    #         self.houses[i] = [self.deck.pop() for _ in range(house_card_counts[9 - i])]
+
+    #     # Deal remaining cards from left to right
+    #     current_house = 0
+    #     while self.deck:
+    #         if len(self.houses[current_house]) < 8:
+    #             self.houses[current_house].append(self.deck.pop())
+    #         current_house = (current_house + 1) % 10
 
     def display_cards(self):
         self.game_canvas.delete("card")
@@ -156,7 +212,7 @@ class PatienceGame:
         for house_idx, house in enumerate(self.houses):
             x = 60 + house_idx * x_spacing
             y = 20
-            for card in house:
+            for card_idx, card in enumerate(house):
                 item = self.game_canvas.create_image(
                     x, y, image=card.image, anchor=tk.NW, tags="card"
                 )
@@ -166,7 +222,12 @@ class PatienceGame:
                     item, "<ButtonRelease-1>", self.on_card_release
                 )
                 self.game_canvas.tag_bind(item, "<B1-Motion>", self.on_card_motion)
-                y += y_spacing
+
+                # Only the top card of each pile should be fully visible
+                if card_idx < len(house) - 1:
+                    y += 15  # Smaller vertical spacing for stacked cards
+                else:
+                    y += y_spacing  # Larger spacing for the top card
 
         # Display cards in end houses
         for house_idx, house in enumerate(self.end_houses):
@@ -181,8 +242,41 @@ class PatienceGame:
 
     def on_card_press(self, event):
         item = self.game_canvas.find_closest(event.x, event.y)[0]
-        self.game_canvas.tag_raise(item)
-        self.drag_data = {"x": event.x, "y": event.y, "item": item}
+        card = self.card_items[item]
+        house_index, house = self.find_card_house(card)
+        if house:
+            card_index = house.index(card)
+            self.drag_data = {
+                "x": event.x,
+                "y": event.y,
+                "item": item,
+                "cards": house[card_index:],
+                "source_house": house,
+                "source_index": card_index,
+            }
+            for drag_card in self.drag_data["cards"]:
+                self.game_canvas.tag_raise(self.get_card_item(drag_card))
+
+    def update_game_state(self, dragged_item):
+        dragged_card = self.card_items[dragged_item]
+        x, y = self.game_canvas.coords(dragged_item)
+
+        # Find the target house
+        target_house_index = min(9, max(0, int((x - 60) / (self.card_width + 20))))
+        target_house = self.houses[target_house_index]
+
+        source_house = self.drag_data["source_house"]
+        source_index = self.drag_data["source_index"]
+
+        if source_house != target_house and self.is_valid_move(
+            dragged_card, target_house
+        ):
+            cards_to_move = source_house[source_index:]
+            self.move_card(dragged_card, source_house, target_house)
+            self.display_cards()  # Redraw all cards
+        else:
+            # If the move is invalid, return the cards to their original position
+            self.display_cards()
 
     def on_card_release(self, event):
         if self.drag_data["item"]:
@@ -195,7 +289,9 @@ class PatienceGame:
         if self.drag_data["item"]:
             dx = event.x - self.drag_data["x"]
             dy = event.y - self.drag_data["y"]
-            self.game_canvas.move(self.drag_data["item"], dx, dy)
+            for card in self.drag_data["cards"]:
+                item = self.get_card_item(card)
+                self.game_canvas.move(item, dx, dy)
             self.drag_data["x"] = event.x
             self.drag_data["y"] = event.y
 
@@ -204,7 +300,20 @@ class PatienceGame:
             return card.rank == 13  # Only King can be placed on an empty house
 
         target_card = target_house[-1]
-        return (card.rank == target_card.rank - 1) and (card.color != target_card.color)
+
+        # Check if the bottom card of the moving unit can be placed on the target card
+        if (card.rank == target_card.rank - 1) and (card.color != target_card.color):
+            # If it's a valid move, check if all cards in the unit maintain the alternating color and descending rank
+            current = card
+            for above_card in card.cards_above:
+                if not (
+                    above_card.rank == current.rank - 1
+                    and above_card.color != current.color
+                ):
+                    return False
+                current = above_card
+            return True
+        return False
 
     def find_card_house(self, card):
         for i, house in enumerate(self.houses):
@@ -213,33 +322,35 @@ class PatienceGame:
         return None, None
 
     def move_card(self, card, from_house, to_house):
-        # Remove the card and all cards on top of it from the source house
+        # Find the index of the card in the source house
         index = from_house.index(card)
+
+        # Get all cards from this card to the top of the pile
         cards_to_move = from_house[index:]
+
+        # Remove these cards from the source house
         from_house[index:] = []
+
+        # If the target house is not empty, update the cards_above of the top card
+        if to_house:
+            to_house[-1].cards_above = cards_to_move
 
         # Add the cards to the target house
         to_house.extend(cards_to_move)
 
-    def update_game_state(self, dragged_item):
-        dragged_card = self.card_items[dragged_item]
-        x, y = self.game_canvas.coords(dragged_item)
+        # Clear the cards_above for all moved cards except the bottom one
+        for moved_card in cards_to_move[:-1]:
+            moved_card.cards_above = []
 
-        # Find the target house
-        target_house_index = min(9, max(0, int((x - 60) / (self.card_width + 20))))
-        target_house = self.houses[target_house_index]
-
-        # Find the source house
-        source_house_index, source_house = self.find_card_house(dragged_card)
-
-        if source_house_index != target_house_index and self.is_valid_move(
-            dragged_card, target_house
-        ):
-            self.move_card(dragged_card, source_house, target_house)
-            self.display_cards()  # Redraw all cards
-        else:
-            # If the move is invalid, return the card to its original position
-            self.display_cards()
+    def on_card_motion(self, event):
+        if self.drag_data["item"]:
+            dx = event.x - self.drag_data["x"]
+            dy = event.y - self.drag_data["y"]
+            for card in self.drag_data["cards"]:
+                item = self.get_card_item(card)
+                self.game_canvas.move(item, dx, dy)
+            self.drag_data["x"] = event.x
+            self.drag_data["y"] = event.y
 
     def check_win(self):
         for house in self.end_houses:
@@ -252,6 +363,12 @@ class PatienceGame:
             if not all(house[i].color != house[i - 1].color for i in range(1, 13)):
                 return False
         return True
+
+    def get_card_item(self, card):
+        for item, c in self.card_items.items():
+            if c == card:
+                return item
+        return None
 
 
 if __name__ == "__main__":
