352df5682b6b2e1c0ccf94dd40a077e485085cf8
Namit Arjaria <namitarjaria25@gmail.com>
2024-07-29 01:06:18 +0530

    chore: Update build-and-release.yml to include release files for different platforms and fix move counter
    Signed-off-by: Namit Arjaria <namitarjaria25@gmail.com>

--- game.py
+++ game.py
@@ -12,7 +12,7 @@ from updater import Updater
 from tkinter import messagebox
 from win_celebration import create_win_celebration
 
-CURRENT_VERSION = "v1.0.17-alpha"
+CURRENT_VERSION = "v1.0.20-alpha"
 
 
 class Card:
